41552ee78664df9a7b697fd6280ff03ad9894f33
maesterfox <talentedmrfox@hotmail.com>
2023-07-19 19:42:30 +0100

    Portfolio mess about

---
+++ js/two.js
@@ -0,0 +1,11 @@
+ document.write(Number.MAX_VALUE+"Is the maximumnumber in JS.<br />");
+ document.write(Number.MIN_VALUE+"Is the minimum number in JS.<br />");
+
+
+ var samp_str = "Here are some escaped characters \"\' \\ \t \n";
+ document.write(samp_str + <br />);
+ /* these samples of escaped characters
+ blah blah blah
+ */
+
+ 
